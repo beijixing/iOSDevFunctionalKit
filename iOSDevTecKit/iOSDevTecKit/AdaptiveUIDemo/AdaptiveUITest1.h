d42f64258fe5c68922d31a70fae855cf1fdd76fd
beijixing <long_555@126.com>
2017-05-17 16:42:59 +0800

    指纹解锁测试

---
+++ iOSDevTecKit/iOSDevTecKit/AdaptiveUIDemo/AdaptiveUITest1.h
@@ -0,0 +1,13 @@
+//
+//  AdaptiveUITest1.h
+//  iOSDevTecKit
+//
+//  Created by 郑光龙 on 2017/1/15.
+//  Copyright © 2017年 郑光龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdaptiveUITest1 : UIViewController
+
+@end
